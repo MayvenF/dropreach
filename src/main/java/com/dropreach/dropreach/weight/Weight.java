fca88c80c51fd9f6ff512f88cc8773e8ab2b9d02
maeve <maeven.fanebust@gmail.com>
2024-10-07 10:12:39 +0300

    Talking to Postgres!

--- src/main/java/com/dropreach/dropreach/weight/Weight.java
+++ src/main/java/com/dropreach/dropreach/weight/Weight.java
@@ -17,32 +17,32 @@ import java.time.LocalDateTime;
 
 // because I marked Entity here, it creates a table of Weights
 @Entity
-@Table(name = "WEIGHT")
+@Table(name = "weight")
 public class Weight{
-        @Id
-        @NotNull
-        private LocalDateTime dateRecorded;
+    @Id
+    @NotNull
+    private LocalDateTime dateRecorded;
 
-        @Positive
-        @NotNull
-        private Float weight;
+    @Positive
+    @NotNull
+    private Float weight;
 
 
-        public LocalDateTime getDateRecorded(){
-                return this.dateRecorded;
-        }
+    public LocalDateTime getDateRecorded(){
+        return this.dateRecorded;
+    }
 
-        public void setDateRecorded(LocalDateTime ldt){
-                this.dateRecorded = ldt;
-        }
+    public void setDateRecorded(LocalDateTime ldt){
+        this.dateRecorded = ldt;
+    }
 
-        public Float getWeight(){
-                return this.weight;
-        }
+    public Float getWeight(){
+        return this.weight;
+    }
 
-        public void setWeight(Float weight){
-                this.weight = weight;
-        }
+    public void setWeight(Float weight){
+        this.weight = weight;
+    }
 
 
 }
